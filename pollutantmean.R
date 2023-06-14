4b5f55e27f91cf67193554c64da066395b06d30b
GitRyers <112345567+GitRyers@users.noreply.github.com>
2023-06-14 16:40:30 -0700

    final working version of pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,11 +1,17 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
+        path <- file.path("C:","Users","ryang", "OneDrive","Documents","R","Coursera", directory, fsep="/")
+        setwd(path)
+        fileNames <- list.files(path)
         sum <- 0
         count <- 0
-        for() {
-                file <- file.path("C:","Users","ryang", "OneDrive","Documents","R","Coursera", directory, fsep="/")
+        for(x in id) {
+                file <- fileNames[x]
                 table <- read.csv(file)
                 pol <- table[pollutant]
                 pol_no_NA <- pol[!is.na(pol)]
-                
+                sum <- sum + sum(pol_no_NA)
+                count <- count + length(pol_no_NA)
         }
+        mean = sum / count
+        mean
 }
