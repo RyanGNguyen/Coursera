f84719375b337d65acba39a7f69da1b00e617a0a
GitRyers <112345567+GitRyers@users.noreply.github.com>
2023-06-14 13:37:06 -0700

    Introduced file iteration and subsetting

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,5 +1,11 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-        file <- file.path("C:","Users","ryang", "OneDrive","Documents","R","Coursera", directory, fsep="/")
-        read.csv(file)
-        
+        sum <- 0
+        count <- 0
+        for() {
+                file <- file.path("C:","Users","ryang", "OneDrive","Documents","R","Coursera", directory, fsep="/")
+                table <- read.csv(file)
+                pol <- table[pollutant]
+                pol_no_NA <- pol[!is.na(pol)]
+                
+        }
 }
