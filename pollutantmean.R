7eadd92b5f4a8a56e6058e535111c3a81ff85398
GitRyers <112345567+GitRyers@users.noreply.github.com>
2023-06-14 12:38:36 -0700

    Initial commit of pollutantmean.R with basic file loading

---
+++ pollutantmean.R
@@ -0,0 +1,5 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+        file <- file.path("C:","Users","ryang", "OneDrive","Documents","R","Coursera", directory, fsep="/")
+        read.csv(file)
+        
+}
