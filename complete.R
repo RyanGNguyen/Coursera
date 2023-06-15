13b96a49487c19fb3ad24580d7e599237a1e0485
GitRyers <112345567+GitRyers@users.noreply.github.com>
2023-06-14 23:12:59 -0700

    Changed ids from row names to distinct column inside dataframe

--- complete.R
+++ complete.R
@@ -1,18 +1,20 @@
 complete <- function(directory, id = 1:332) {
-        path <- file.path("C:","Users","ryang", "OneDrive","Documents","R","Coursera", directory, fsep="/")
+        path <- file.path("C:","Users","ryang", "OneDrive","Documents","R",
+                          "Coursera", directory, fsep="/")
         setwd(path)
         fileNames <- list.files(path)
+        idList <- c()
         nobs <- c()
-        files <- c()
         for(x in id) {
                 file <- fileNames[x]
                 table <- read.csv(file)
                 num <- sum(!is.na(table[2:3]))
-                files <- c(files, x)
+                idList <- c(idList, x)
                 nobs <- c(nobs, num)
         }
-        df <- data.frame(nobs, row.names = files)
-        names(df) <- c("id", "nobs")
+        values <- cbind(idList, nobs)
+        df <- data.frame(values)
+        colnames(df) <- c('id', 'nobs')
         df
         
 }
