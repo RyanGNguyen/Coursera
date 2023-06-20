a75475bafd361fd6b59a79cbcc777d0132b7ca1c
GitRyers <112345567+GitRyers@users.noreply.github.com>
2023-06-20 15:13:25 -0700

    First draft of best.R

---
+++ best.R
@@ -0,0 +1,9 @@
+best <- function(state, outcome) {
+        ##Read outcome data 
+        data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+        
+        ##Check that state and outcome are valid 
+        if (is.null(data[data$State == state]) )
+        
+        ##Return hospital name in that state with lowest 30-day death rate
+}
