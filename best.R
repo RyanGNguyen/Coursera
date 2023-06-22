0cdce9508f32cad718258b071b90cf0fdd87b677
GitRyers <112345567+GitRyers@users.noreply.github.com>
2023-06-22 11:58:04 -0700

    Implemented parameter validation

--- best.R
+++ best.R
@@ -1,9 +1,17 @@
 best <- function(state, outcome) {
         ##Read outcome data 
         data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+        outcomes <- c("heart attack", "heart failure", "pneumonia")
+        states <- unique(data$State)
         
         ##Check that state and outcome are valid 
-        if (is.null(data[data$State == state]) )
+        if (!(state %in% states)) {
+                print("invalid state")
+        }
+        
+        if (!(outcome %in% outcomes)) {
+                print("invalid outcome")
+        }
         
         ##Return hospital name in that state with lowest 30-day death rate
 }
