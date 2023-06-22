84061d0d8dc63be32e87801b9e9b0ed092aac6ec
GitRyers <112345567+GitRyers@users.noreply.github.com>
2023-06-22 15:01:25 -0700

    First draft of full script

--- best.R
+++ best.R
@@ -4,14 +4,28 @@ best <- function(state, outcome) {
         outcomes <- c("heart attack", "heart failure", "pneumonia")
         states <- unique(data$State)
         
-        ##Check that state and outcome are valid 
+        ##Check if state is valid
         if (!(state %in% states)) {
                 print("invalid state")
         }
         
+        ##Check if outcome is valid
         if (!(outcome %in% outcomes)) {
                 print("invalid outcome")
         }
         
+        ##Finalize search range
+        search_out <- if (identical(outcome, outcomes[1])) {
+                "Heart.Attack"
+        } else if (identical(outcome, outcomes[2])) {
+                "Heart.Failure"
+        } else {
+                "Pneumonia"
+        }
+        search_col <- paste("Hospital.30.Day.Death..Mortality..Rates.from", 
+                             search_out, sep = ".") 
+        hospitals <- data[data$State == state, search_col]
+        
         ##Return hospital name in that state with lowest 30-day death rate
+        data[which(hospitals == min(hospitals)), "Hospital.Name"]
 }
